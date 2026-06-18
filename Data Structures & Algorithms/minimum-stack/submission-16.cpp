9ce7dde8c954f1827b89211435072f329370a396
K1nja1 <kinjal619hey@gmail.com>
2026-06-19 00:17:32 +0530

    Add: minimum-stack - submission-16

---
+++ Data Structures & Algorithms/minimum-stack/submission-16.cpp
@@ -0,0 +1,33 @@
+class MinStack {
+public:
+    vector<long long> st;
+    long long minel=INT_MAX;
+    MinStack() {}
+    
+    void push(int val) {
+        if(st.empty()) {st.push_back(val); minel=val;}
+        else if (val>=minel) st.push_back(val);
+        else {
+            long long encoded= 2LL*val-minel;
+            st.push_back(encoded);
+            minel=val;
+        }
+
+    }
+    
+    void pop() {
+        long long top=st.back();
+        st.pop_back();
+        if(top<minel) minel=2*minel-top;
+    }
+    
+    int top() {
+        if(st.back()<minel) return minel;
+        else return st.back();
+    }
+    
+    int getMin() {
+        if(!st.empty()) return minel;
+        else return -1;
+    }
+};
